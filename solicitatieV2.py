431b7a2908fb97ad55f3148aeb5af8acf971d944
EnesT51 <99040669@mydavinci.nl>
2021-09-22 09:38:00 +0200

    Solicitatie

--- solicitatieV2.py
+++ solicitatieV2.py
@@ -5,6 +5,7 @@ diploma = input('heeft u een MBO-4 ondernemings diploma? ')
 Truck_license = input('bent u in bezit van een geldige vrachtwagenrijbewijs? ')
 hoed = input('bent u in bezit van een hoge hoed? ')  
 geslacht = input('bent u een man of een vrouw? ')
+Certificaat = 'ja'
 redHair = 'ja'
 lengte = 150
 gewicht = 90
@@ -12,7 +13,9 @@ snor = 'ja'
 snor2 = 0
 
 if geslacht == 'man':
-    lengte = int(input('hoelang bent u? '))
+    Certificaat = input('heeft u een certificaat? ')
+    if Certificaat == 'ja':
+        lengte = int(input('hoelang bent u? '))
     if lengte >= 150:
         snor = input('heeft u een snor? ')
     if snor == 'ja':
@@ -22,7 +25,9 @@ if geslacht == 'man':
          snor2 = 0
 
 if geslacht =='vrouw':
-    lengte = int(input('hoelang bent u? '))
+    Certificaat =input('heeft u een certificaat? ')
+    if Certificaat =='ja':
+        lengte = int(input('hoelang bent u? '))
     if lengte >= 150:
         redHair = input('heeft u rood krullig haar? ')
     if redHair == 'ja':
@@ -30,10 +35,10 @@ if geslacht =='vrouw':
 else:
     haarlengte = 0
 
-if (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3)and(gewicht >=90 and lengte >=150 and diploma =='ja' and Truck_license == 'ja' and hoed == 'ja' and(geslacht == 'man' and snor == 'ja' and snor2 >=10) or(redHair =='ja'and haarlengte >=20 and geslacht =='vrouw')): 
+if (dieren_dressuur >= 4 or jongleren >= 5 or acrobatiek >= 3)and(gewicht >=90 and lengte >=150 and diploma =='ja' and Truck_license == 'ja' and hoed == 'ja' and(geslacht == 'man' and snor == 'ja' and snor2 >=10 and Certificaat == 'ja') or(redHair =='ja'and Certificaat == 'ja'and haarlengte >=20 and geslacht =='vrouw')): 
     print('gefeliciteerd u bent aangenomen!!')
 else:
-    print('helaas, u voldoet niet aan de eisen')
+    print('helaas, u voldoet niet aan de eisen') 
 
 
 
